0f3c33015df347068f8aa1e9a2a97273e0e4c695
Dhanush8 <kasundanushka503@gmail.com>
2021-07-27 21:34:07 +0530

    Initial Commit

---
+++ project.py
@@ -0,0 +1,12 @@
+x=float(input('Input the first value: '))
+y=float(input('Input the secound value: '))
+
+sum=x+y
+sub=x-y
+mul=x*y
+div=x/y
+
+print('Sum:',sum)
+print('Sub:',sub)
+print('Mul:',mul)
+print('Div:',div)
